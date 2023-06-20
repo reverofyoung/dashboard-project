d832230087e55e93d5a28b34011f06d630f8a702
260 <reverofyoung@gmail.com>
2023-06-20 11:01:56 +0900

    레포지토리 연결

---
+++ src/components/Dashboard.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const ClockCon = styled.div`
+  align-items: center;
+  background-color: skyblue;
+  display: flex;
+  justify-content: center;
+`;
+
+const ClockStyle = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const UserCon = styled.div`
+
+`;
+
+const UserInput = styled.input`
+  width: 100%;
+`;
+
+const SaveButton = styled.button`
+
+`;
+
+
+function Dashboard() {
+  const [time, setTime] = useState(new Date());
+
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return (() => 
+      clearInterval(id) // 불필요한 작업 방지
+    ); 
+  }, []);
+
+  const getUserName = (e) => {
+    setUserName(e.target.value);
+  };
+
+  return (
+    <div>
+      <ClockCon>
+        <ClockStyle>{ time.toLocaleString() }</ClockStyle>
+      </ClockCon>
+
+      <UserCon>
+        <UserInput 
+          onChange={ getUserName } 
+          placeholder="닉네임을 입력해주세요😁" 
+          value={ userName } 
+        />
+        <SaveButton title="저장" />
+      </UserCon>
+    </div>
+   
+  );
+}
+
+export default Dashboard;
